c10a83b63e3e7526792cf1e4b77ebe2697685955
Brian Colfer <brian.colfer@gmail.com>
2020-09-07 13:40:59 -0700

    Added fast_json serializer This provides better and easier customization of the json data structure

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,41 @@
 # frozen_string_literal: true
+# In Sessions Controller block
+# before_action :configure_sign_in_params, only: [:create]
 
+# GET /resource/sign_in
+# def new
+#   super
+# end
+
+# POST /resource/sign_in
+# def create
+#   super
+# end
+
+# DELETE /resource/sign_out
+# def destroy
+#   super
+# end
+
+# protected
+
+# If you have extra params to permit, append them to the sanitizer.
+# def configure_sign_in_params
+#   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+# end
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  respond_to :json
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  private
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def respond_with(resource, _opts = {})
+    render json: {
+        status: {code: 200, msessage: 'Logged in successfully.'},
+        data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+    }
+  end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def respond_to_on_destroy
+    head :ok
+  end
 end
